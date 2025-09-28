0e077ca279c81db82a47063d0a8815e4b24a4085
wiilff <mattiswulff@gmail.com>
2025-09-28 13:00:01 +0200

    cors

--- src/main/java/com/wilff/gains_spring/config/WebConfig.java
+++ src/main/java/com/wilff/gains_spring/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://68.183.65.231")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
